de212bb90ea4d217ac3b226fa39d575f8a88b01e
NuwanJ <nuwanjaliyagoda@gmail.com>
2021-09-16 15:33:53 +0530

    Added PlatformIO firmware folder

---
+++ firmware/src/main.cpp
@@ -0,0 +1,45 @@
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DHTPIN 2     
+// Uncomment the type of sensor in use:
+#define DHTTYPE    DHT11     // DHT 11
+// #define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+uint32_t delayMS;
+
+void setup() {
+  Serial.begin(9600);
+  // Initialize device.
+  dht.begin();
+}
+
+void loop() {
+  // Delay between measurements.
+  delay(1000);
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
+}
---
+++ firmware/test/test.cpp
@@ -0,0 +1,62 @@
+#include <Arduino.h>
+#include <unity.h>
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DHTPIN 2     
+#define DHTTYPE    DHT11     // DHT 11
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+void test_dht11(void){
+    sensors_event_t event;
+    dht.temperature().getEvent(&event);
+    TEST_ASSERT_NOT_EQUAL(true, isnan(event.temperature));
+}
+
+void test_led_builtin_pin_number(void) {
+    TEST_ASSERT_EQUAL(13, LED_BUILTIN);
+}
+
+void test_led_state_high(void) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    TEST_ASSERT_EQUAL(HIGH, digitalRead(LED_BUILTIN));
+}
+
+void test_led_state_low(void) {
+    digitalWrite(LED_BUILTIN, LOW);
+    TEST_ASSERT_EQUAL(LOW, digitalRead(LED_BUILTIN));
+}
+
+void setup() {
+    // NOTE!!! Wait for >2 secs
+    // if board doesn't support software reset via Serial.DTR/RTS
+    delay(2000);
+
+    UNITY_BEGIN();    // IMPORTANT LINE!
+    RUN_TEST(test_led_builtin_pin_number);
+
+    dht.begin();
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+uint8_t i = 0;
+uint8_t max_blinks = 5;
+
+void loop() {
+    if (i < max_blinks)
+    {
+        RUN_TEST(test_dht11);
+
+        RUN_TEST(test_led_state_high);
+        delay(500);
+        RUN_TEST(test_led_state_low);
+        delay(500);
+        i++;
+    }
+    else if (i == max_blinks) {
+      UNITY_END(); // stop unit testing
+    }
+}
